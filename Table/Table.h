de28de846defd691778b1f160e7a3c27b2b3bb65
Brad Hilton <brad.hilton.nw@gmail.com>
2016-11-22 09:03:08 -0700

    Initial Commit

---
+++ Table/Table.h
@@ -0,0 +1,19 @@
+//
+//  Table.h
+//  Table
+//
+//  Created by Bradley Hilton on 11/22/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Table.
+FOUNDATION_EXPORT double TableVersionNumber;
+
+//! Project version string for Table.
+FOUNDATION_EXPORT const unsigned char TableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Table/PublicHeader.h>
+
+
